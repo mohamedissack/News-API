68a00e5bf7a5120f7825e082581f140ff18c3250
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-11-02 08:49:41 +0300

    add class news article

--- app/models.py
+++ app/models.py
@@ -9,3 +9,15 @@ class News:
         self.url = url
         self.category = category
         self.country = country
+
+
+class NewsArticle:
+    def __init__(self,id,author,title,description,url,urlToImage,publishedAt,content):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content        
