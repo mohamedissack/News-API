627b037894b382fbbf7d07efa8744374a809301e
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-11-02 05:55:59 +0300

    add class config

--- config.py
+++ config.py
@@ -6,3 +6,12 @@ class Config:
     NEWS_ARTICLE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey=a45ed0dd525f493fba9a73353bda0b46'
 
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
