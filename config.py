995215ea9e60a22e04711e2bc85ae383151552d4
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-11-02 05:53:03 +0300

    add basic and article url

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 import os
 
 class Config:
+
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&apiKey=a45ed0dd525f493fba9a73353bda0b46'
+    NEWS_ARTICLE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey=a45ed0dd525f493fba9a73353bda0b46'
