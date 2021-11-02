d127a72176d41a7edeed8b530ab6a1c5e53096e4
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-11-02 05:54:28 +0300

    add news api

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ class Config:
 
     NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&apiKey=a45ed0dd525f493fba9a73353bda0b46'
     NEWS_ARTICLE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey=a45ed0dd525f493fba9a73353bda0b46'
+
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
